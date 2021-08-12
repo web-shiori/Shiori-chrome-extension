811e4ec83b89f1293cf85f52382449600d7c18e5
masatora <atarashi.masatora@gmail.com>
2021-08-13 03:41:12 +0900

    コンテンツを複数個表示する

--- src/ts/popup/content.ts
+++ src/ts/popup/content.ts
@@ -11,7 +11,7 @@ interface content {
 }
 
 const content_1: content = {
-    title: "title",
+    title: "title1",
     url: "https://www.youtube.com/watch?v=pBtcTIDsS24&ab_channel=AbleNet",
     thumbnail_img_url: "https://i.ytimg.com/vi/xP_Ovd8-GM8/maxresdefault.jpg",
     registered_at: "2019-05-12T20:48:24.000+09:00",
@@ -22,21 +22,44 @@ const content_1: content = {
     video_playback_position: 500,
 }
 
+const content_2: content = {
+    title: "title2",
+    url: "https://www.youtube.com/watch?v=pBtcTIDsS24&ab_channel=AbleNet",
+    thumbnail_img_url: "https://i.ytimg.com/vi/xP_Ovd8-GM8/maxresdefault.jpg",
+    registered_at: "2019-05-12T20:48:24.000+09:00",
+    scroll_position_x: 0,
+    scroll_position_y: 500,
+    max_scroll_position_x: 1000,
+    max_scroll_position_y: 1000,
+    video_playback_position: 500,
+}
+
+const content_list = [content_1, content_2]
+
 //TODO: サムネイルをurlから取得する
 //TODO: 登録日を加工する
-const view = `
-<h4>保存済みのコンテンツ</h4>
-<div>
-<img src="${content_1.thumbnail_img_url}" height="50px" width="50px">
-<h6>${content_1.title}</h6>
-<h6>${content_1.url}</h6>
-<h6>${content_1.registered_at}</h6>
-</div>
-`;
+
+let content_view = ``
+
+for (const content of content_list) {
+    const view = `
+    <h4>保存済みのコンテンツ</h4>
+    <div>
+    <img src="${content.thumbnail_img_url}" height="50px" width="50px">
+    <h6>${content.title}</h6>
+    <h6>${content.url}</h6>
+    <h6>${content.registered_at}</h6>
+    </div>
+    <hr>
+    `
+
+    content_view += view
+}
+
 
 const content_list_view = document.getElementById("content-list-view");
 if (content_list_view !== null) {
-    content_list_view.innerHTML = view;
+    content_list_view.innerHTML = content_view;
 } else {
     alert(document.documentElement.innerHTML)
 }
