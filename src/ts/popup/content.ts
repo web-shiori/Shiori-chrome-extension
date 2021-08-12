65679b4392d3cdda4f3a56892246a40659b677e2
masatora <atarashi.masatora@gmail.com>
2021-08-13 04:27:53 +0900

    コンテンツをクリックできるようにする

--- src/ts/popup/content.ts
+++ src/ts/popup/content.ts
@@ -1,3 +1,5 @@
+// TODO: デフォルトのサムネイルurlを定数で持っておく
+
 interface content {
     title: string
     url: string
@@ -33,18 +35,18 @@ const content_2: content = {
     max_scroll_position_y: 1000,
     video_playback_position: 500,
 }
-
 const content_list = [content_1, content_2]
 
-//TODO: サムネイルをurlから取得する
-//TODO: 登録日を加工する
-
-let content_view = ``
+let content_view_tl = `
+    <h4>保存済みのコンテンツ</h4>
+    <hr>
+`
 
 for (const content of content_list) {
-    const view = `
-    <h4>保存済みのコンテンツ</h4>
-    <div>
+    //TODO: サムネイルをurlから取得する
+    //TODO: 登録日を加工する
+    const view_tl = `
+    <div class="content-view">
     <img src="${content.thumbnail_img_url}" height="50px" width="50px">
     <h6>${content.title}</h6>
     <h6>${content.url}</h6>
@@ -52,16 +54,22 @@ for (const content of content_list) {
     </div>
     <hr>
     `
-
-    content_view += view
+    content_view_tl += view_tl
 }
-
 
 const content_list_view = document.getElementById("content-list-view");
 if (content_list_view !== null) {
-    content_list_view.innerHTML = content_view;
+    content_list_view.innerHTML = content_view_tl;
+
+    const content_view = document.getElementsByClassName('content-view');
+    for(var i = 0; i < content_view.length; i++) {
+        (function(index) {
+            content_view[index].addEventListener("click", function() {
+                alert("unko")
+            })
+        })(i);
+    }
 } else {
     alert(document.documentElement.innerHTML)
 }
-
 
