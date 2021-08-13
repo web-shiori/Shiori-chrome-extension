c814126d42eb54b3ef058a18de3c5228be6f3249
masatora <atarashi.masatora@gmail.com>
2021-08-14 00:08:15 +0900

    保存するボタンを押したときの処理を作成

---
+++ src/ts/popup/popup.ts
@@ -0,0 +1,8 @@
+const save_button = document.getElementById("save-button");
+if (save_button !== null) {
+    save_button.addEventListener('click', function () {
+        alert("click")
+    })
+}
+
+
