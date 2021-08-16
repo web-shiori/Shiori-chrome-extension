78d6aaeef1455b7e3057fd94ac57810c10a410f7
masatora <atarashi.masatora@gmail.com>
2021-08-17 06:36:36 +0900

    検索結果を表示する

--- src/ts/contentList/header.ts
+++ src/ts/contentList/header.ts
@@ -1,9 +1,28 @@
 module contentList {
+
+    function getSearchQuery(): Promise<string> {
+        const searchQuery = (<HTMLInputElement>document.getElementById("search-text-field")).value || ""
+        return new Promise<string>((resolve => {
+            resolve(searchQuery)
+        }))
+    }
+
+    const searchForm = document.getElementById("search-form")
+    if (searchForm !== null) {
+        searchForm.onsubmit = function () {
+            getSearchQuery().then((searchQuery => {
+                initializeContent(searchQuery, currentFolderId)
+            }))
+            // ページのリロードを防ぐ
+            return false;
+        }
+    }
+
     //NOTE: もっとかんたんにやる方法ある気がする。formのAPIを調べる
     const searchButton = document.getElementById("search-button")
     if (searchButton !== null) {
-        searchButton.addEventListener("click", function () {
-            const searchQuery = (<HTMLInputElement>document.getElementById("search-text-field")).value || ""
+        searchButton.addEventListener("click", async function () {
+            const searchQuery = await getSearchQuery()
             initializeContent(searchQuery, currentFolderId)
         })
     }
